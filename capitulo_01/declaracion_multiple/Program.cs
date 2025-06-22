6157258492c614eeafdfc4ff36e7e4b6a85cb42c
Alexander <alexander5243188@yahoo.es>
2025-06-22 12:15:27 -0400

    Declaracion de varias variables del mismo tipo en una linea

---
+++ capitulo_01/declaracion_multiple/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int valor1 = 10, valor2 = 20, valor3 = 30;
+        int suma = valor1 + valor2 + valor3;
+        Console.WriteLine($"{valor1} + {valor2} + {valor3} = {suma}");
+    }
+}
