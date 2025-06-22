66afea88ee8d534094e66d57ad089f323064f895
Alexander <alexander5243188@yahoo.es>
2025-06-22 10:45:28 -0400

    Declaracion y uso de datos primitivos

---
+++ capitulo_01/datos_primitivos/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int edad = 22;
+        float altura = 1.60f;
+        double peso = 45.5;
+        char inicial = 'D';
+        string nombre = "Daniela";
+        bool esEstudiante = false;
+        Console.WriteLine($"Hola mi nombre es, {nombre} tengo {edad}, una altura de {altura} metros, peso {peso} kilos, mi inicial es {inicial} y es estudiante: {esEstudiante} ");
+    }
+}
