314c267038ed1d6545bc243339231e59a9a235c5
Alexander <alexander5243188@yahoo.es>
2025-06-21 21:18:41 -0400

    suma de dos valores

---
+++ capitulo_01/suma_valores/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var primerValor = 5;
+        var segundoValor = 10;
+        var resultado = primerValor + segundoValor;
+        Console.WriteLine($"{primerValor} + {segundoValor} = {resultado}");
+      }
+}
