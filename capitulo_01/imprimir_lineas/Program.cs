39dced5c6ad5002884819b79ddac577f62791e9d
Alexander <alexander5243188@yahoo.es>
2025-06-21 21:23:59 -0400

    imprimir dos lineas

---
+++ capitulo_01/imprimir_lineas/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var primerMensaje = "Bienvenido a C#";
+        var segundoValor = "estoy aprendiendo a programar";
+        Console.WriteLine($"{primerMensaje}");
+        Console.WriteLine($"{segundoValor}");
+    }
+}
