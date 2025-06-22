10857ce226bbceb4a7d8136ad79909e010bca984
Alexander <alexander5243188@yahoo.es>
2025-06-21 21:10:56 -0400

    Mostrar la edad

---
+++ capitulo_01/edad_actual/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    { 
+        var birthYear = 1979;
+        var currentYear = 2025;
+        var age = currentYear - birthYear;
+
+        Console.WriteLine($"You are {age} years old.");
+    }
+}
